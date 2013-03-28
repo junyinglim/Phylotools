3d04ab38c8aac35c714c3db8f33a08b863b178f6
Junying Lim <junyinglim@gmail.com>
2013-03-28 20:42:03 +0000

    Created an argument to input backbone newick

--- taxo2newick.R
+++ taxo2newick.R
@@ -1,3 +1,5 @@
+#newick and taxo2newick
+
 newick <- function(x){
   base <- x[1]
   if(length(x) > 1){
@@ -9,18 +11,26 @@ newick <- function(x){
     return(base)
   }
 }
-paste("(", base, ")", sep = "")
 
-taxo2newick <-function(x){
-  base <- newick(unique(x$Class))
-  for(i in 1:length(unique(x$Class))){
-    temp <- subset(x, Class == unique(x$Class)[i])
-    temp2 <- newick(as.vector(unique(temp$Order)))
-    base <- gsub(unique(x$Class)[i], temp2, base)
-    for(j in 1:length(unique(temp$Order))){
-      lowertemp <- subset(temp, Order == unique(temp$Order)[j])
-      lowertemp2 <- newick(as.vector(unique(lowertemp$Family)))
-      base <- gsub(unique(temp$Order)[j], lowertemp2, base)
+taxo2newick <-function(x, backbone = TRUE, backbonenewick){
+  taxo <- c("order", "class", "family")
+  if(sum(match(tolower(names(x)), taxo, nomatch=0)) == 0){
+    stop("Dataframe does not have taxonomic classifications")
+  }else{
+    if(backbone == TRUE){
+      base <- backbonenewick
+    } else {
+      base <- newick(unique(x$Class))  
+    }
+    for(i in 1:length(unique(x$Class))){
+      temp <- subset(x, Class == unique(x$Class)[i])
+      temp2 <- newick(as.vector(unique(temp$Order)))
+      base <- gsub(unique(x$Class)[i], temp2, base)
+      for(j in 1:length(unique(temp$Order))){
+        lowertemp <- subset(temp, Order == unique(temp$Order)[j])
+        lowertemp2 <- newick(as.vector(unique(lowertemp$Family)))
+        base <- gsub(unique(temp$Order)[j], lowertemp2, base)
+      }
     }
   }
   return(paste(base, ";", ""))
