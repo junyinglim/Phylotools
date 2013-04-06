4f0b03d2c751a1720418c73d028a466b275905fe
Junying Lim <junyinglim@gmail.com>
2013-04-06 19:15:22 +0100

    Improved modularity and flexibility of code
    Less hard-coding, rewrote code so code can be used in a modular way to
    suit different taxonomic needs

--- taxo2newick.R
+++ taxo2newick.R
@@ -1,4 +1,8 @@
 #newick and taxo2newick
+#newick returns a list of taxa names into a polytomic newick tree
+#taxo2newick returns a dataframe of taxa names into a newick tree,
+#assuming monophyly in the penultimate taxonomic hierarchy (e.g., if data
+#is on genera, then confamilial genera will be a monophyletic polytomy)
 
 newick <- function(x){
   base <- x[1]
@@ -12,31 +16,30 @@ newick <- function(x){
   }
 }
 
-taxo2newick <-function(x, backbone = TRUE, backbonenewick){
+taxo2newick <-function(x, backbone = TRUE, backbonenewick, hightaxo, lowtaxo){
   taxo <- c("order", "class", "family")
   if(sum(match(tolower(names(x)), taxo, nomatch=0)) == 0){
     stop("Dataframe does not have taxonomic classifications")
   }else{
+    hightaxoindex <- match(hightaxo, names(x))   #Find index of desired taxa
+    hightaxolist <- as.vector(unique(x[, hightaxoindex]))   #Figure out what are the levels in the particular
     if(backbone == TRUE){
       base <- backbonenewick
     } else {
-    base <- newick(unique(x$Class))
+      base <- newick(hightaxolist)
     }
-    for(i in 1:length(unique(x$Class))){
-      temp <- subset(x, Class == unique(x$Class)[i])
-      temp2 <- newick(as.vector(unique(temp$Order)))
-      base <- gsub(unique(x$Class)[i], temp2, base)
-      for(j in 1:length(unique(temp$Order))){
-        lowertemp <- subset(temp, Order == unique(temp$Order)[j])
-        lowertemp2 <- newick(as.vector(unique(lowertemp$Family)))
-        base <- gsub(unique(temp$Order)[j], lowertemp2, base)
-      }
+    for(i in 1:length(hightaxolist)){
+      temp <- subset(x, x[,hightaxoindex] == hightaxolist[i]) #Subsets dataframe based on higher taxonomy to find the lower taxonomic levels
+      lowtaxolist <- as.vector(unique(temp[,match(lowtaxo, names(temp))]))
+      temp2 <- newick(lowtaxolist)
+      base <- gsub(hightaxolist[i], temp2, base)
+      print(base)
     }
   }
-  return(paste(base, ";", ""))
+  return(base)
 }
 
 #TESTING
 taxo <- data.frame(Class = c("A", "A", "B", "B", "C"), Order = c("D", "E", "F", "G", "H"), Family = c("I","J", "K", "L", "M"))
-noconst <- taxo2newick(taxo)
-const <- taxo2newick(taxo, backbone = TRUE, backbonenewick = "((A, C), B)")
+classorder <- taxo2newick(taxo, backbone = FALSE, hightaxo = "Class", lowtaxo = "Order")
+orderfamily <- taxo2newick(taxo, backbone = TRUE, hightaxo = "Order", lowtaxo = "Family", backbonenewick = classorder)
